9cd92840c0f29f21b98fed7df675c8e733c2061e
Parwesh Rallapalli <parweshrallapalli@gmail.com>
2019-10-04 22:51:01 -0400

    BeautifulSoup is now scraping Case, Brody, and Shaw menus while also updating based on date and time

--- menu-app.py
+++ menu-app.py
@@ -1,23 +1,36 @@
 from flask import Flask, render_template
 from bs4 import BeautifulSoup
+from datetime import date, datetime
 import urllib3
 
 ##### BeautifulSoup4 #####
 def make_soup(url):
     http = urllib3.PoolManager()
     response = http.request("GET", url)
-    soup = BeautifulSoup(response.data, 'html.parser')
-    return soup
+    return BeautifulSoup(response.data, 'html.parser')
 
-date = "2019-10-05"
-CASE_URL = f"https://eatatstate.msu.edu/menu/South%20Pointe%20at%20Case/all/{date}"
-soup = make_soup(CASE_URL)
+#date = "2019-10-05"
+today = date.today()
+date = today.strftime("%Y-%m-%d") # get date from system
+
+dining_hall_dict = {}
+dining_hall_dict["CASE"] = f"https://eatatstate.msu.edu/menu/South%20Pointe%20at%20Case/all/{date}"
+dining_hall_dict["BRODY"] = f"https://eatatstate.msu.edu/menu/Brody%20Square/all/{date}"
+dining_hall_dict["SHAW"] = f"https://eatatstate.msu.edu/menu/The%20Vista%20at%20Shaw/all/{date}"
+
+now = datetime.now()
+
 meal = "breakfast"
-results = soup.find_all("div", class_=f"meal-title {meal}")
-for item in results:
-    print(item.string)
 
-'''
+print(f"[{date}]")
+for hall in dining_hall_dict:
+    print(f"{hall} BREAKFAST\n-----------")
+    soup = make_soup(dining_hall_dict[hall])
+    results = soup.find_all("div", class_=f"meal-title {meal}")
+    for item in results:
+        print(item.string)
+    print()
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -30,4 +43,3 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
-'''
