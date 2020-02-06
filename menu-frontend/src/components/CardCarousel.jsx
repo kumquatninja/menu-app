6c2cb24296b1890f558ea45207c831ab6ad85ebe
Parwesh <parweshrallapalli@gmail.com>
2020-02-06 13:42:36 -0500

    Added custom stylesheet with margins between cards and horizontal scrolling. Added date rendering

--- menu-frontend/src/components/CardCarousel.jsx
+++ menu-frontend/src/components/CardCarousel.jsx
@@ -1,18 +1,31 @@
 import React, { Component } from "react";
 import Card from "./Card";
+import "../custom.css";
 
 class CardCarousel extends Component {
   state = {
     halls: [
       { id: "AKERS", items: 0 },
       { id: "CASE", items: 0 },
+      { id: "BRODY", items: 0 },
+      { id: "SHAW", items: 0 },
       { id: "BRODY", items: 0 }
     ]
   };
+
+  ShowCurrentDate = () => {
+    var date = new Date().getDate();
+    var month = new Date().getMonth() + 1;
+    var year = new Date().getFullYear();
+
+    return month + "-" + date + "-" + year;
+  };
+
   render() {
     return (
       <div className="container-fluid py-2">
         <h2 class="font-weight-light">Menu App</h2>
+        <h4 class="font-weight-light">{this.ShowCurrentDate()}</h4>
         <div className="d-flex flex-row flex-nowrap">
           {this.state.halls.map(hall => (
             <Card id={hall.id} />
