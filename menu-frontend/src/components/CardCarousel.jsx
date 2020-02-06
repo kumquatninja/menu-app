ad3ae1201617763751252ef9627d8f6a4a2da14a
Parwesh <parweshrallapalli@gmail.com>
2020-02-06 12:59:02 -0500

    Akers, Case, and Brody now are all dynamically displayed. Refactored React DOM

--- menu-frontend/src/components/CardCarousel.jsx
+++ menu-frontend/src/components/CardCarousel.jsx
@@ -2,15 +2,21 @@ import React, { Component } from "react";
 import Card from "./Card";
 
 class CardCarousel extends Component {
+  state = {
+    halls: [
+      { id: "AKERS", items: 0 },
+      { id: "CASE", items: 0 },
+      { id: "BRODY", items: 0 }
+    ]
+  };
   render() {
     return (
       <div className="container-fluid py-2">
         <h2 class="font-weight-light">Menu App</h2>
         <div className="d-flex flex-row flex-nowrap">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {this.state.halls.map(hall => (
+            <Card id={hall.id} />
+          ))}
         </div>
       </div>
     );
--- menu-frontend/src/components/card.jsx
+++ menu-frontend/src/components/card.jsx
@@ -2,13 +2,11 @@ import React, { Component } from "react";
 // remember to uninstall axios module
 
 class Card extends Component {
-  constructor() {
-    super();
-    this.state = {
-      hall: "akers",
-      akersBreakfast: []
-    };
-  }
+  state = {
+    hall: this.props.id,
+    day: "day_0",
+    items: []
+  };
 
   componentDidMount() {
     fetch("http://menu-app-msu.appspot.com/menus")
@@ -16,29 +14,28 @@ class Card extends Component {
         return response.json();
       })
       .then(data => {
+        let hallName = this.state.hall;
+        let day = this.state.day;
+
+        console.log("hall", hallName);
+
         this.setState({
-          akersBreakfast: JSON.parse(JSON.parse(data["day_0"])["AKERS"])[
-            "breakfast"
-          ]
+          items: JSON.parse(JSON.parse(data[day])[hallName])["breakfast"]
         });
         console.log(this.state);
       });
   }
 
   render() {
-    let items = this.state.akersBreakfast.map(item => (
-      <li key={item}>{item}</li>
-    ));
+    let items = this.state.items.map(item => <li key={item}>{item}</li>);
     console.log(items);
     return (
-      <div>
-        <div className="card" width="18rem">
-          <div className="card-body">
-            <h5 className="card-title text-center">Akers</h5>
-            <p className="card-text">
-              <ul>{items}</ul>
-            </p>
-          </div>
+      <div className="card col-4">
+        <div className="card-body">
+          <h5 className="card-title text-center">{this.state.hall}</h5>
+          <p className="card-text">
+            <ul>{items}</ul>
+          </p>
         </div>
       </div>
     );
